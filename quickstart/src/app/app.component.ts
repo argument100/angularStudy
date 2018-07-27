9444b818705834daa2efd1000060b31532162247
ono <100blendy@gmail.com>
2018-07-28 00:59:46 +0900

    セクション５　レクチャー２１まで

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -19,68 +19,84 @@ const MEMBERS: Member[] = [
 ];
 
 @Component({
-  selector: 'my-app',
+  selector: "my-app",
   template: `
     <h1>{{title}}</h1>
     <h2>社員一覧</h2>
     <ul class="members">
-      <li *ngFor="let member of members">
+      <li *ngFor="let member of members" (click)="onSelect(member)" [class.selected]="member === selectedMember">
         <span class="badge">{{member.id}}</span> {{member.name}}
       </li>
     </ul>
+
+    <div *ngIf="selectedMember">
+      <h2>{{selectedMember.name}}</h2>
+      <div><label>id: </label>{{selectedMember.id}}</div>
+      <div>
+        <label>name: </label>
+        <input type="text" [(ngModel)]="selectedMember.name" placeholder="名前">
+      </div>
+    </div>
   `,
 
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .members {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .members li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .5em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .members li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .members li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .members .text {
-      position: relative;
-      top: -3px;
-    }
-    .members .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `]
+  styles: [
+    `
+      .selected {
+        background-color: #cfd8dc !important;
+        color: white;
+      }
+      .members {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 15em;
+      }
+      .members li {
+        cursor: pointer;
+        position: relative;
+        left: 0;
+        background-color: #eee;
+        margin: 0.5em;
+        padding: 0.3em 0;
+        height: 1.6em;
+        border-radius: 4px;
+      }
+      .members li.selected:hover {
+        background-color: #bbd8dc !important;
+        color: white;
+      }
+      .members li:hover {
+        color: #607d8b;
+        background-color: #ddd;
+        left: 0.1em;
+      }
+      .members .text {
+        position: relative;
+        top: -3px;
+      }
+      .members .badge {
+        display: inline-block;
+        font-size: small;
+        color: white;
+        padding: 0.8em 0.7em 0 0.7em;
+        background-color: #607d8b;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: 0.8em;
+        border-radius: 4px 0 0 4px;
+      }
+    `
+  ]
 })
-export class AppComponent  {
-  title = '自社社員名簿';
+export class AppComponent {
+  title = "自社社員名簿";
   members = MEMBERS;
+  selectedMember: Member;
+
+  onSelect(member: Member): void {
+    this.selectedMember = member;
+  }
 }
